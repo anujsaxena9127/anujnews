1ad895de76f8914764832fd5403cb18c4a4772d9
anujsaxena9127 <anujsaxena9127@gmail.com>
2020-06-21 15:44:42 +0530

    breaking news updated

--- anujnews/views.py
+++ anujnews/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    import requests
+    import json
+    news_api_request=requests.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=3c2e1471a6514464a290269dd41f753b")
+    api=json.loads(news_api_request.content)
+    return render(request, 'index.html',{'api':api})
